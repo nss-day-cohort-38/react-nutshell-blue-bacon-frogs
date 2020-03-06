41e90f944c1ab38f7d4e89ce994a90120180b650
Andrew Green <green.andrew.b@gmail.com>
2020-03-06 09:10:32 -0600

    logout button added

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,45 +1,48 @@
-import React, {useState} from "react"
+import React, { useState } from "react";
 
-const Login = (props) => {
-    const [credentials, setCredentials] = useState({email: "", password: ""}) //initial state equal to an object with keys email and password that have empty string value
-    //handleFieldChange handles each state update and targets the values of email and password
-    const handleFieldChange = (evt) => {
-        //a variable which takes in email and password
-        const stateToChange = {...credentials}
-        stateToChange[evt.target.id] = evt.target.value
-        //the state of the credentials changes to the value of email and password
-        setCredentials(stateToChange)
-    }
-    const handleLogin = (evt) => {
-        evt.preventDefault();
-        //props from appviews to set user equal to the value of credentials
-        props.setUser(credentials)
-        //props from route 
-        props.history.push("/home")
-    }
+const Login = props => {
+  const [credentials, setCredentials] = useState({ email: "", password: "" }); //initial state equal to an object with keys email and password that have empty string value
+  //handleFieldChange handles each state update and targets the values of email and password
+  const handleFieldChange = evt => {
+    //a variable which takes in email and password
+    const stateToChange = { ...credentials };
+    stateToChange[evt.target.id] = evt.target.value;
+    //the state of the credentials changes to the value of email and password
+    setCredentials(stateToChange);
+  };
+  const handleLogin = evt => {
+    evt.preventDefault();
+    //props from appviews to set user equal to the value of credentials
+    props.setUser(credentials);
+    //props from route
+    props.history.push("/home");
+  };
 
-    return (
-        <form onSubmit={handleLogin}>
-            <fieldset>
-            <h3>Sign in</h3>
-            <input onChange={handleFieldChange} 
-            type="email" 
-            id="email"
-            placeholder="email address"></input>
-            <label htmlFor="inputEmail">Email Address</label>
-            </fieldset>
-            <fieldset>
-            <input onChange={handleFieldChange} 
-            type="password"
-            id="password"
-            placeholder="password"
-            ></input>
-            <label htmlFor="inputPassword">Password</label>
-            </fieldset>
+  return (
+    <form onSubmit={handleLogin}>
+      <fieldset>
+        <h3>Sign in</h3>
+        <input
+          onChange={handleFieldChange}
+          type="email"
+          id="email"
+          placeholder="email address"
+        ></input>
+        <label htmlFor="inputEmail">Email Address</label>
+      </fieldset>
+      <fieldset>
+        <input
+          onChange={handleFieldChange}
+          type="password"
+          id="password"
+          placeholder="password"
+        ></input>
+        <label htmlFor="inputPassword">Password</label>
+        <h3> </h3>
         <button type="submit">Submit</button>
-        </form>
-    )
+      </fieldset>
+    </form>
+  );
+};
 
-}
-
-export default Login
+export default Login;
--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,7 +1,14 @@
 import React from "react"
 import {Link} from "react-router-dom"
+import { withRouter } from 'react-router-dom';
+
 //if the Navbar prop hasUser exists ( there is a user ) render the nav bar
 const NavBar = (props) => {
+    const handleLogout = () => {
+        props.clearUser();
+        props.history.push('/login');
+      }
+      
     return (
         <nav>     
 {props.hasUser ? (
@@ -22,6 +29,9 @@ const NavBar = (props) => {
                 <li>
                     <Link className="nav-link" to="/friendships">Friends</Link>
                 </li>
+                <li>
+                    <span className="nav-link" onClick={handleLogout}> Logout </span>
+                </li>
             </ul>
     ): null}
         </nav>
@@ -29,4 +39,4 @@ const NavBar = (props) => {
     )
 }
 
-export default NavBar
+export default withRouter(NavBar)
