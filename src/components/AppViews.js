16760a845961e78314068aeb91063dcce9915625
Cody Murdock <warhammer4563@gmail.com>
2020-03-09 10:08:40 -0500

    initial commit

--- src/components/AppViews.js
+++ src/components/AppViews.js
@@ -1,25 +1,61 @@
-import React from "react"
-import {Route, Redirect} from "react-router-dom"
-import Login from "./auth/Login"
-import Home from "./home/Home"
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import Login from "./auth/Login";
+import Home from "./home/Home";
+import ArticleList from "./articles/ArticleList";
+import ArticleEditForm from "./articles/ArticleEditForm";
+import ArticleForm from "./articles/ArticleForm"
 
-const AppViews = (props) => {
-    const hasUser = props.hasUser;
-    const setUser = props.setUser;
-    return (
+const AppViews = props => {
+  const hasUser = props.hasUser;
+  const setUser = props.setUser;
+  return (
     <React.Fragment>
-        <Route path="/login" render={props => {  
-        return <Login setUser={setUser} {...props} />
-        }}/> 
-        <Route path="/" render={props => {
-            if(hasUser) {
-                return <Home />
-            } else {
-            return <Redirect to="/login"/>
-            }
-        }}/>
+      <Route
+        path="/login"
+        render={props => {
+          return <Login setUser={setUser} {...props} />;
+        }}
+      />
+      <Route
+        path="/"
+        render={props => {
+          if (hasUser) {
+            return <Home />;
+          } else {
+            return <Redirect to="/login" />;
+          }
+        }}
+      />
+      <Route
+        exact
+        path="/articles"
+        render={props => {
+          if (hasUser) {
+            return <ArticleList {...props} />;
+          } else {
+            return <Redirect to="/login" />;
+          }
+        }}
+      />
+      <Route
+        path="/articles/:articleId(\d+)/edit"
+        render={props => {
+          if (hasUser) {
+            return <ArticleEditForm {...props} />;
+          } else {
+            return <Redirect to="/login" />;
+          }
+        }}
+      />
+      <Route
+        path="/articles/new"
+        render={props => {
+          return <ArticleForm {...props} />;
+        }}
+      />
     </React.Fragment>
-    )
-}
+  );
+};
 
-export default AppViews
+export default AppViews;
---
+++ src/modules/ApiManager.js
@@ -0,0 +1,33 @@
+const apiURL = "http://localhost:5002/";
+const API = {
+    get(str) {
+        return fetch(apiURL + str).then(entries => entries.json());
+    },
+    save(objToSave, str) {
+        return fetch(apiURL + str, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(objToSave)
+        })
+    },
+    delete(objToDeleteId, str) {
+        return fetch(`${apiURL}${str}/${objToDeleteId}`, {
+            method: "DELETE"
+        });
+    },
+    edit(objToEditId, str) {
+        return fetch(apiURL + str + "/" + objToEditId).then(entry => entry.json());
+    },
+    update(objToEdit, str) {
+        return fetch(`${apiURL}${str}/${objToEdit.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(objToEdit)
+        });
+    }
+}
+export default API;
