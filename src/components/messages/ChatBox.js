e80a5106c2daed143ce4a1d6448b75a6a9b3fe9c
Michael Clark <michaelclarknashville@protonmail.com>
2020-03-09 15:32:23 -0500

    names added to chat messages

--- src/components/messages/ChatBox.js
+++ src/components/messages/ChatBox.js
@@ -1,42 +1,47 @@
-import API from "../../modules/ApiManager"
-import React, { useState, useEffect } from "react";
-import ChatMessage from "./ChatMessage";
-import ChatInput from "./ChatInput"
+import API from "../../modules/ApiManager"
+import React, { useState, useEffect } from "react";
+import ChatMessage from "./ChatMessage";
+import ChatInput from "./ChatInput"
 
-const ChatBox = (props) => {
-  const [messages, setMessages] = useState([]);
+const ChatBox = (props) => {
+  const [messages, setMessages] = useState([]);
 
-  const getMessages = () => {
-    return API.get("messages", "user").then(messagesFromAPI => {
-      setMessages(messagesFromAPI);
-    });
-  };
+  
 
-  useEffect(() => {
-    getMessages();
+  const getMessages = () => {
+    return API.expand("messages", "user").then(messagesFromAPI => {
+      setMessages(messagesFromAPI);
+    });
+  };
 
-  }, []);
+  useEffect(() => {
+    getMessages();
 
-  return (
-    <>
-      <div className="entireChat">
-        <div id="chatScrollBox">
-          {messages.map(message => (
-            <ChatMessage
-              key={message.id}
-              message={message}
-              {...props}
-            />
-          ))}
-        </div>
-        <div id="chatInput">
-          <ChatInput
-            userId={props.userId}
-            getMessages={getMessages}
-            {...props} />
-        </div>
-      </div>
-    </>
-  );
+  }, []);
+
+  return (
+    <>
+      <div className="entireChat">
+        <div id="chatScrollBox">
+          {messages.map(message => (
+            <ChatMessage
+              key={message.id}
+              name={message.user.username}
+
+              userId={props.userId}
+              message={message}
+              {...props}
+            />
+          ))}
+        </div>
+        <div id="chatInput">
+          <ChatInput
+            userId={props.userId}
+            getMessages={getMessages}
+            {...props} />
+        </div>
+      </div>
+    </>
+  );
 };
-export default ChatBox;
+export default ChatBox;
--- src/components/messages/ChatMessage.js
+++ src/components/messages/ChatMessage.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import API from "../../modules/ApiManager";
 
 const ChatMessage = props => {
   console.log(props.message.id);
+  
+  let name = ""
+
+  API.getWithId("users", props.userId)
+  .then(user => {
+    name = user.username
+  })
 
   return (
     <div className="chatMessage">
-      {props.message.id}: {props.message.message}
+      {props.name}: {props.message.message}
       <button
         type="button"
         // onClick={() => props.history.push(`/animals/${props.message.id}/edit`)}
