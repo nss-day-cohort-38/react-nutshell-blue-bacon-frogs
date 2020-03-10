62bba735780ff979d4d2d1bfe3887a77af570e87
sncandiani <sofiancandiani@gmail.com>
2020-03-10 14:54:19 -0500

    making more changes to home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,18 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
+import API from "../../modules/ApiManager"
 import TaskCard from "../tasks/TaskCard";
 import TaskList from "../tasks/TaskList"
 
 const Home = (props) => {
-    /* const [tasks, setTasks] = useState([])
+    const[task, setTask] = useState()
+
+    
+    const someTask = () => {
+
+    }
 return (
-    {tasks.map(task => <TaskCard task={task}/>)}
-) */
-<h1> WOrld</h1>
+    <h1>Something</h1>
+) 
 }
 
 export default Home
